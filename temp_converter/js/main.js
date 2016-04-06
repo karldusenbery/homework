ab9c6404a122dc865f63379ab4bf0163fe32e3a4
karldusenbery <karl.dusenbery>
2016-04-05 21:21:33 -0700

    started assignment in class

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,13 @@
+// When the user clicks on the "F to C" button 
+$('#fahrenheit_to_celsius').on('click', function () {
+	// Get the value of #temperature and store it in a new varible called userTemp
 
+	// convert the varible userTemp to the datatye number (parseFloat)
+
+	//actually convert the tempurature from Farenheit to Celcius, and store it in a new varible called Celcuis
+
+	/*display the tempurature that the user inputted in Celcius as #result
+		on the left it will display userTemp
+		on the right it will display the converyed tempurature
+	*/
+});
