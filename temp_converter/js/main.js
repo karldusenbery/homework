74c5dbacc6c006bde465842db845ff198c540cff
karldusenbery <karl.dusenbery>
2016-04-07 15:15:30 -0700

    Added validation to the form

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -2,21 +2,42 @@
 $('#fahrenheit_to_celsius').on('click', function () {
 	// GET the value of #temperature and associate it with a varible called userTemp.
 	var userTemp = $('#temperature').val();
+	console.log("value of userTemp right before parseFloat: " + userTemp);
 
-	// convert the varible userTemp to the datatye number (parseFloat) with 2 decimal places.
-	userTemp = parseFloat(userTemp).toFixed(2);
-	console.log("value of userTemp right after parseFloat: " + userTemp);
-	console.log("The data type of userTemp is: " + (typeof 'userTemp'));
+	if (userTemp === "") {
 
-	//if ((typeof 'userTemp') === "string")
+		if ( $( "#error" ).hasClass( "errorMessage") ) {
+			$( "#error" ).hide()
+			$( "#temperature" ).addClass( "error").fadeIn(500);
+		}
+		else {
+			$( "#temperature" ).addClass( "error").fadeIn(500);
+		}
+	}
 
-	//actually convert the tempurature from Fahrenheit to Celsius, and store it in a new varible called convertedTemp
-	var convertedTemp = ((userTemp - 32) / 1.8); 
-	console.log("The data type of convertedTemp is: " + (typeof 'convertedTemp'));
-	/*display the tempurature that the user inputted in Celcius as #result
-		on the left it will display userTemp (Fahrenheit temp value)
-		on the right it will display the converted tempurature in Celsius
-	*/
-	$('#result').html(userTemp + " Fahrenheit is " + convertedTemp + " Celsius.");
+	else {
+		$( "#temperature" ).removeClass( "error")
+		// convert the varible userTemp to the datatye number (parseFloat) with 2 decimal places.
+		userTemp = parseFloat(userTemp).toFixed(2);
+		console.log("value of userTemp right after parseFloat: " + userTemp);
 
+		if (userTemp === "NaN") {
+			$( "#error" ).addClass( "errorMessage").fadeIn(500);
+			$( "#temperature" ).addClass( "error").fadeIn(500)
+		}
+
+		
+
+		else {
+			//actually convert the tempurature from Fahrenheit to Celsius, and store it in a new varible called convertedTemp
+			var convertedTemp = ((userTemp - 32) / 1.8); 
+
+			/*display the tempurature that the user inputted in Celcius as #result
+			on the left it will display userTemp (Fahrenheit temp value)
+			on the right it will display the converted tempurature in Celsius
+			*/
+			$( "#error" ).hide()
+			$('#result').html(userTemp + " Fahrenheit is " + convertedTemp + " Celsius.");	
+		}
+	}
 });
