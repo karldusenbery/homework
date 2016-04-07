e4425c0ad7ad5c14ffd0eb4fbf2e419124d4f1b2
karldusenbery <karl.dusenbery>
2016-04-07 10:48:52 -0700

    completed F to C button functionality

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,13 +1,21 @@
 // When the user clicks on the "F to C" button 
 $('#fahrenheit_to_celsius').on('click', function () {
-	// Get the value of #temperature and store it in a new varible called userTemp
+	// GET the value of #temperature and associate it with a varible called userTemp.
+	var userTemp = $('#temperature').val();
+
+	//log the value of the userTemp variable to the console
+		//console.log(userTemp);
 
 	// convert the varible userTemp to the datatye number (parseFloat)
+	parseFloat(userTemp);
 
-	//actually convert the tempurature from Farenheit to Celcius, and store it in a new varible called Celcuis
+	//actually convert the tempurature from Fahrenheit to Celsius, and store it in a new varible called convertedTemp
+	var convertedTemp = ((userTemp - 32) / 1.8); 
 
 	/*display the tempurature that the user inputted in Celcius as #result
-		on the left it will display userTemp
-		on the right it will display the converyed tempurature
+		on the left it will display userTemp (Fahrenheit temp value)
+		on the right it will display the converted tempurature in Celsius
 	*/
+	$('#result').html(userTemp + " Fahrenheit is " + convertedTemp + " Celsius.");
+
 });
