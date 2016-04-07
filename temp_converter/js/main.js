18033566814107514a2482ce0c145432ca2722ee
karldusenbery <karl.dusenbery>
2016-04-07 11:16:34 -0700

    fixed parseFloat functionality
    and added 2 decimal places.
    And added debugging console logs

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -3,15 +3,16 @@ $('#fahrenheit_to_celsius').on('click', function () {
 	// GET the value of #temperature and associate it with a varible called userTemp.
 	var userTemp = $('#temperature').val();
 
-	//log the value of the userTemp variable to the console
-		//console.log(userTemp);
+	// convert the varible userTemp to the datatye number (parseFloat) with 2 decimal places.
+	userTemp = parseFloat(userTemp).toFixed(2);
+	console.log("value of userTemp right after parseFloat: " + userTemp);
+	console.log("The data type of userTemp is: " + (typeof 'userTemp'));
 
-	// convert the varible userTemp to the datatye number (parseFloat)
-	parseFloat(userTemp);
+	//if ((typeof 'userTemp') === "string")
 
 	//actually convert the tempurature from Fahrenheit to Celsius, and store it in a new varible called convertedTemp
 	var convertedTemp = ((userTemp - 32) / 1.8); 
-
+	console.log("The data type of convertedTemp is: " + (typeof 'convertedTemp'));
 	/*display the tempurature that the user inputted in Celcius as #result
 		on the left it will display userTemp (Fahrenheit temp value)
 		on the right it will display the converted tempurature in Celsius
