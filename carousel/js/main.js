2f90cbb1c27d037e7af0dd758f5b8300d4200564
karldusenbery <karl.dusenbery>
2016-04-12 21:10:28 -0700

    initial commit
    started pseudocode

---
+++ carousel/js/main.js
@@ -0,0 +1,15 @@
+//create an array of image paths
+
+//set a varible for our current position
+car currentPosition = 0;
+
+// Advance to the next image when user clicks on the next button
+	//add 1 to the currentPosition
+
+	//if currentPosition the last image, disable the next button.
+
+	//change the src pf #image-to-vote-on to image at that index
+
+// Advance to the previous image when user clicks on the previous button
+// When the user clicks on the thumbsUp button, add a like on the current image.
+// When the user clicks on the thumbsDown button, remove a like on the current image.
