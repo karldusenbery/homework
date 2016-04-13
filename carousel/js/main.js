942b21519d1de39b3fe4e8be5687cb75938ee3ae
karldusenbery <karl.dusenbery>
2016-04-13 15:43:53 -0700

    started buttons functionality
    not done yet

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -6,16 +6,11 @@ var currentPosition = 0;
 // Advance to the next image when user clicks on the next button
 $('#next').on('click', function() {
 
-	//If currentPosition is first image, disable the Previous button.
-	if (currentPosition === 0 ){
-		//disable the Next button
-		$('#prev').attr('disabled', 'disabled');
-	} else {
-		$('#prev').attr('enabled', 'enabled');
-	}
-
 	//add 1 to the currentPosition
 	currentPosition += 1;
+
+	//enable the Previous button
+	$('#prev').removeAttr('disabled');
 
 	//if currentPosition is last image, disable the Next button.
 	if (currentPosition === (images.length - 1 )) {
@@ -23,14 +18,25 @@ $('#next').on('click', function() {
 		$('#next').attr('disabled', 'disabled');
 	}
 
-
 	//change the src of #image-to-vote-on to image at that index
 	$('#image-to-vote-on').attr('src', images[currentPosition]);
 });
 
 
-// Advance to the previous image when user clicks on the previous button
+// Go to to the previous image when user clicks on the previous button
+$('#prev').on('click', function() {
 
+	//subtract 1 to the currentPosition
+	currentPosition -= 1;
+
+	//If the currentPosition is not the first image, enabel the Previous button
+	if (currentPosition > 0){
+		$('#prev').removeAttr('disabled');
+	}
+
+	//change the src of #image-to-vote-on to image at that index
+	$('#image-to-vote-on').attr('src', images[currentPosition]);
+});
 
 // When the user clicks on the thumbsUp button, add a like on the current image.
 // When the user clicks on the thumbsDown button, remove a like on the current image.
