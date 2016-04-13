dd69425d7180276f69ddcb87c3084e33bb63b038
karldusenbery <karl.dusenbery>
2016-04-13 15:14:42 -0700

    completed Next button functionality

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -5,10 +5,25 @@ var currentPosition = 0;
 
 // Advance to the next image when user clicks on the next button
 $('#next').on('click', function() {
+
+	//If currentPosition is first image, disable the Previous button.
+	if (currentPosition === 0 ){
+		//disable the Next button
+		$('#prev').attr('disabled', 'disabled');
+	} else {
+		$('#prev').attr('enabled', 'enabled');
+	}
+
+
 	//add 1 to the currentPosition
 	currentPosition += 1;
 
-	//if currentPosition the last image, disable the next button.
+	//if currentPosition is last image, disable the Next button.
+	if (currentPosition === (images.length)) {
+		//disable the Next button
+		$('#next').attr('disabled', 'disabled');
+	}
+
 
 	//change the src of #image-to-vote-on to image at that index
 	$('#image-to-vote-on').attr('src', images[currentPosition]);
